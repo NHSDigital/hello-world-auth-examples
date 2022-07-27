45bb880d3191d772deab6da9fbd387b8a4db9f3f
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2022-07-27 14:28:34 +0100

    AMB-1214: Added Java example for API-Key App.Restricted Auth.

---
+++ application-restricted-api-key-tutorials/java/src/main/java/nhsd/apim/App.java
@@ -0,0 +1,17 @@
+package nhsd.apim;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) throws IOException, Exception {
+        String ENDPOINT = System.getenv("ENDPOINT");
+        String API_KEY = System.getenv("API_KEY");
+
+        String response = HelloWorld.makeApplicationRestrictedRequest(ENDPOINT, API_KEY);
+        System.out.println(response);
+    }
+}
