987dddde8daf2c7c21878825fd22ad051c1a84a5
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2022-08-31 12:45:36 +0100

    AMB-1260: Removed dead code from main method

--- user-restricted-combined-auth-tutorials/cis2/java/src/main/java/nhsd/apim/App.java
+++ user-restricted-combined-auth-tutorials/cis2/java/src/main/java/nhsd/apim/App.java
@@ -14,12 +14,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App
 {
     public static void main(String[] args) throws IOException, Exception {
-        String OAUTH_ENDPOINT = System.getenv("OAUTH_ENDPOINT");
-        String CLIENT_ID = System.getenv("CLIENT_ID");
-        String CLIENT_SECRET = System.getenv("CLIENT_SECRET");
-        String REDIRECT_URI = System.getenv("REDIRECT_URI");
-        String ENDPOINT = System.getenv("ENDPOINT");
-
         System.out.println("User Restricted App.java");
 
         SpringApplication.run(App.class, args);
