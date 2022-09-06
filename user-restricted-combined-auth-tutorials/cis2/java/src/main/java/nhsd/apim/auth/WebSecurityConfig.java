eb7932e103149ad80ec7d549461743a4c9304b4a
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2022-09-06 15:47:31 +0100

    AMB-1260: Fixed Spring Security failing to authenticate due to missing UserInfo.
    This fix relies on providing `application.yml` with `user-info-uri` and `userNameAttribute` values, we're passing the `/userinfo` endpoint for this purpose - but this only works for CIS2, since NHS Login doesn't have this endpoint available.
    
    For NHS Login we'll likely have to grab the ID token from Apigee somehow and create a custom AuthenticationProvider similar to `OidcAuthorizationCodeAuthenticationProvider` to grab the user properties Spring Security needs from this ID token.

--- user-restricted-combined-auth-tutorials/cis2/java/src/main/java/nhsd/apim/auth/WebSecurityConfig.java
+++ user-restricted-combined-auth-tutorials/cis2/java/src/main/java/nhsd/apim/auth/WebSecurityConfig.java
@@ -4,20 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    private CustomTokenRequest customTokenRequest;
-
-    public WebSecurityConfig(CustomTokenRequest customTokenRequest) {
-        this.customTokenRequest = customTokenRequest;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,9 +18,6 @@ public class WebSecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .oauth2Login();
-//                ).oauth2Login(oauth2Login ->
-//                        oauth2Login.tokenEndpoint(tokenEndpoint ->
-//                                tokenEndpoint.accessTokenResponseClient(customTokenRequest)));
 
         return http.build();
     }
--- user-restricted-combined-auth-tutorials/cis2/java/src/main/java/nhsd/apim/controllers/MainController.java
+++ user-restricted-combined-auth-tutorials/cis2/java/src/main/java/nhsd/apim/controllers/MainController.java
@@ -1,29 +1,19 @@
 package nhsd.apim.controllers;
 
 import nhsd.apim.hello.HelloWorld;
-import nhsd.apim.auth.Auth;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Controller
 public class MainController {
-
-    private final String OAUTH_ENDPOINT = System.getenv("OAUTH_ENDPOINT");
-    private final String CLIENT_ID = System.getenv("CLIENT_ID");
-    private final String CLIENT_SECRET = System.getenv("CLIENT_SECRET");
-    private final String REDIRECT_URI = System.getenv("REDIRECT_URI");
     private final String ENDPOINT = System.getenv("ENDPOINT");
 
     @GetMapping("/error")
@@ -36,35 +26,12 @@ public class MainController {
         return "index"; //view
     }
 
-//    @GetMapping("/auth")
-//    public void authUser(HttpServletResponse response) throws Exception {
-//        // Build the URL parameters needed for authorization
-//        String authURL = Auth.buildAuthorizationURL(OAUTH_ENDPOINT + "/authorize", CLIENT_ID, REDIRECT_URI);
-//        // Redirect to the external authorization endpoint and get the user to sign in with CIS2
-//        response.setHeader("Location", authURL);
-//        response.setStatus(302);
-//    }
-
-    @GetMapping("/callback")
-    public void authRedirect(@RequestParam("code") String code, @RequestParam("state") String state ,HttpServletResponse response, Model model) throws Exception {
-        model.addAttribute("code", code);
-
-        JSONObject tokenResponse = Auth.getTokenResponse(OAUTH_ENDPOINT + "/token", CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, code);
-        String accessToken = tokenResponse.getString("access_token");
-        String expiresIn = tokenResponse.getString("expires_in");
-
-        String redirectParameters = String.join("&",
-                "accessToken=" + accessToken,
-                "expiresIn=" + expiresIn);
-        String redirectURL = "/success?" + redirectParameters;
-
-        response.setHeader("Location", redirectURL);
-        response.setStatus(302);
-    }
-
     @GetMapping("/success")
-    public String authSuccessful(@RequestParam("accessToken") String accessToken, @RequestParam("expiresIn") String expiresIn, Model model) {
-        model.addAttribute("accessToken", accessToken);
+    public String authSuccessful(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client, Model model) {
+        OAuth2AccessToken accessToken = client.getAccessToken();
+        String accessTokenString = accessToken.getTokenValue();
+        String expiresIn = accessToken.getExpiresAt().toString();
+        model.addAttribute("accessToken", accessTokenString);
         model.addAttribute("expiresIn", expiresIn);
         return "auth_successful"; //view
     }
