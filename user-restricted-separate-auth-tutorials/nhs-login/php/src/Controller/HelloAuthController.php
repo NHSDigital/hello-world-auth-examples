67cc96229978ff941da9f52b0c36ccd0166681d9
Andre Silva <silva.andre.daniel@gmail.com>
2022-09-13 16:43:01 +0100

    AMB-1277 php done

--- user-restricted-separate-auth-tutorials/nhs-login/php/src/Controller/HelloAuthController.php
+++ user-restricted-separate-auth-tutorials/nhs-login/php/src/Controller/HelloAuthController.php
@@ -1,16 +1,17 @@
 <?php
 namespace App\Controller;
 
+use App\Service\ISTokenExchange;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\JwtHandler;
+use App\Service\SeparateAuthHttpClient;
+use Error;
 
-
-class HelloAuthController extends AbstractController
-{
+class HelloAuthController extends AbstractController {
     
-
-    #[Route('/')]
+    #[Route(path: '/', name: 'homepage')]
     public function index(): Response
     {
         return $this->render('hellotut/index.html.twig');
@@ -21,15 +22,15 @@ class HelloAuthController extends AbstractController
     public function auth()
     {
         $options = [
-            'scope' => ['nhs-login']
+            'scope' => ['openid']
         ];
 
         $provider = new \League\OAuth2\Client\Provider\GenericProvider([
-            'clientId'                => $_ENV["CLIENT_ID"] ,    // The client ID assigned to you by the provider
-            'clientSecret'            => $_ENV["CLIENT_SECRET"],    // The client password assigned to you by the provider
+            'clientId'                => 'hello-world-tutorials' ,    // The client ID assigned to you by the provider 
             'redirectUri'             => 'http://localhost:8000/hello',
-            'urlAuthorize'            => 'https://sandbox.api.service.nhs.uk/oauth2/authorize',
-            'urlAccessToken'          => 'https://sandbox.api.service.nhs.uk/oauth2/token',
+            'urlAuthorize'            => 'https://identity.ptl.api.platform.nhs.uk/auth/realms/NHS-Login-mock-sandbox/protocol/openid-connect/auth',
+            'urlAccessToken'          => 'https://identity.ptl.api.platform.nhs.uk/auth/realms/NHS-Login-mock-sandbox/protocol/openid-connect/token',
+            'scopes' => ['openid', 'profile'], 
             'urlResourceOwnerDetails' => ''
         ]);
 
@@ -44,46 +45,66 @@ class HelloAuthController extends AbstractController
 
 
     #[Route('/hello')]
-    public function hello_landing(): Response
-    {
-        $provider = new \League\OAuth2\Client\Provider\GenericProvider([
-            'clientId'                => $_ENV["CLIENT_ID"] ,    // The client ID assigned to you by the provider
-            'clientSecret'            => $_ENV["CLIENT_SECRET"],    // The client password assigned to you by the provider
-            'redirectUri'             => 'http://localhost:8000/hello',
-            'urlAuthorize'            => 'https://sandbox.api.service.nhs.uk/oauth2/authorize',
-            'urlAccessToken'          => 'https://sandbox.api.service.nhs.uk/oauth2/token',
-            'urlResourceOwnerDetails' => ''
-        ]);
+    public function callback(): Response {   
+        
+        $code = $_GET['code'];
+
+        $key_path = $_ENV['NHS_LOGIN_KEY_PATH'];
+        $tokenUrl = 'https://identity.ptl.api.platform.nhs.uk/auth/realms/NHS-Login-mock-sandbox/protocol/openid-connect/token';
+        $clientId = $_ENV['NHS_LOGIN_CLIENT_ID'];
+
+        $ISkey_path = $_ENV['IDENTITY_SERVICE_KEY_PATH'];
+        $IStokenUrl = 'https://sandbox.api.service.nhs.uk/oauth2-mock/token';
+        $ISclientId = $_ENV['IDENTITY_SERVICE_CLIENT_ID'];
+
+        error_log("#####################");
+        error_log( $ISkey_path);
+        error_log( $key_path);
+
+        $jwtHandler = new JwtHandler($key_path, $tokenUrl, $clientId, "test-1", 'RS256', 'sha256WithRSAEncryption');
+        $jwt = $jwtHandler->GenerateJwt();
+
 
         try {
-            
-            // Try to get an access token using the authorization code grant.
-            $accessToken = $provider->getAccessToken('authorization_code', [
-                'code' => $_GET['code']
-            ]);
-            
-            // We have an access token, which we may use in authenticated
-            // requests against the service provider's API.
-            $access_token = $accessToken->getToken();
 
+            $separateAuthClient = new SeparateAuthHttpClient($code, $jwt, $tokenUrl);
+            $token = $separateAuthClient->getAccessToken();
+            
+            if( !isset( $token->id_token) ){
+                return $this->redirectToRoute('homepage');
+            }
+            
+            
+            $idToken = $token->id_token;
+            
     
+            
+            $ISJwtHandler = new JwtHandler($ISkey_path, $IStokenUrl, $ISclientId, "test-1", 'RS512', 'sha512WithRSAEncryption');
+            $ISjwt = $ISJwtHandler->GenerateJwt();
+            
+            $ISTokenExchangeHandler = new ISTokenExchange($idToken, $ISjwt);
+            $finalToken = $ISTokenExchangeHandler->tokenExchange();
+            
+            $provider = new \League\OAuth2\Client\Provider\GenericProvider([
+                'urlAuthorize'=> '', 
+                'urlAccessToken'=> '', 
+                'urlResourceOwnerDetails' => ''
+            ]);
             $options['headers']['Content-Type'] = 'application/json';
             $options['headers']['Accept'] = 'application/json';
-            $request = $provider->getAuthenticatedRequest( 'GET', 'https://sandbox.api.service.nhs.uk/hello-world/hello/user', $accessToken, $options );
+            $request = $provider->getAuthenticatedRequest( 'GET', 'https://sandbox.api.service.nhs.uk/hello-world/hello/user', $finalToken, $options );
             $response = $provider->getParsedResponse( $request );
             $html_response = implode('' ,$response);
 
         } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
-
-            // Failed to get the access token or user details.
             exit($e->getMessage());
-
         }
-        
-        return $this->render('hellotut/auth.html.twig', [
-            'number' => $html_response
-        ]);
-    }
 
+        return $this->render('hellotut/auth.html.twig', [
+            'number' => $html_response 
+        ]);
+
+    }
    
 }
+
