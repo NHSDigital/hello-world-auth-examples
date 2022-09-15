c65c9126574f54a83bfafc800dd8230898222a78
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-09-15 21:22:57 +0100

    add auth code flow

---
+++ user-restricted-separate-auth-tutorials/nhs-login/java/src/main/java/nhsd/apim/auth/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package nhsd.apim.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/", "/home", "/error", "/oauth2/authorization/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login();
+
+        return http.build();
+    }
+}
