25dc0b0f90b0a82924a90acd7529636ef462b212
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-09-20 22:07:05 +0100

    AMB-1276 add custom token handler

---
+++ user-restricted-separate-auth-tutorials/nhs-login/java/src/main/java/nhsd/apim/auth/TokenRequestClient.java
@@ -0,0 +1,125 @@
+package nhsd.apim.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationExchange;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+class TokenResponse {
+    @JsonProperty("access_token")
+    public String accessToken;
+    @JsonProperty("refresh_token")
+    public String refreshToken;
+    @JsonProperty("expires_in")
+    public String expiresIn;
+    @JsonProperty("refresh_expires_in")
+    public String refreshExpiresIn;
+    @JsonProperty("token_type")
+    public String tokenType;
+    @JsonProperty("not-before-policy")
+    public String notBeforePolicy;
+    @JsonProperty("session_state")
+    public String sessionState;
+    @JsonProperty("scope")
+    public String scope;
+}
+
+@Component
+public class TokenRequestClient {
+    private final JwtGenerator providerJwtGen;
+    private final JwtGenerator serviceJwtGen;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Value("${auth.provider.kid}")
+    private String providerKid;
+
+    @Value("${auth.service.kid}")
+    private String serviceKid;
+
+    @Value("${auth.service.tokenUri}")
+    private String serviceTokenUri;
+
+    @Value("${auth.service.clientId}")
+    private String serviceClientId;
+
+    public TokenRequestClient(JwtGenerator providerJwtGen, JwtGenerator serviceJwtGen, RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.providerJwtGen = providerJwtGen;
+        this.serviceJwtGen = serviceJwtGen;
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+    public String getToken(OAuth2AuthorizationCodeGrantRequest authorizationGrantRequest) throws JsonProcessingException {
+        String idToken = getTokenRequest(authorizationGrantRequest);
+        String jwt = providerJwtGen.generateJwt(serviceClientId, serviceKid, serviceTokenUri);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add(OAuth2ParameterNames.GRANT_TYPE, "urn:ietf:params:oauth:grant-type:token-exchange");
+        body.add("subject_token", idToken);
+        body.add("subject_token_type", "urn:ietf:params:oauth:token-type:id_token");
+        body.add(OAuth2ParameterNames.CLIENT_ASSERTION_TYPE, "urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
+        body.add(OAuth2ParameterNames.CLIENT_ASSERTION, jwt);
+
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED.toString());
+
+        URI url = UriComponentsBuilder.fromUriString(serviceTokenUri).build().toUri();
+        RequestEntity<MultiValueMap<String, String>> request = new RequestEntity<>(body, headers, HttpMethod.POST, url);
+        ResponseEntity<String> res = restTemplate.exchange(request, String.class);
+
+        return "";
+    }
+
+    private String getTokenRequest(OAuth2AuthorizationCodeGrantRequest authorizationGrantRequest) throws JsonProcessingException {
+        OAuth2AuthorizationExchange authorizationExchange = authorizationGrantRequest.getAuthorizationExchange();
+        ClientRegistration clientRegistration = authorizationGrantRequest.getClientRegistration();
+
+        String tokenUri = authorizationGrantRequest.getClientRegistration().getProviderDetails().getTokenUri();
+        String clientId = clientRegistration.getClientId();
+        // TODO: is it really needed? seems ok without it!
+        String clientSecret = clientRegistration.getClientSecret();
+        String code = authorizationExchange.getAuthorizationResponse().getCode();
+        String redirectUri = authorizationExchange.getAuthorizationRequest().getRedirectUri();
+        String clientAssertionType = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
+        String jwt = providerJwtGen.generateJwt(clientId, providerKid, tokenUri);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add(OAuth2ParameterNames.GRANT_TYPE, "authorization_code");
+        body.add(OAuth2ParameterNames.CODE, code);
+//        body.add(OAuth2ParameterNames.CLIENT_SECRET, clientSecret);
+        body.add(OAuth2ParameterNames.CLIENT_ID, clientId);
+        body.add(OAuth2ParameterNames.REDIRECT_URI, redirectUri);
+        body.add(OAuth2ParameterNames.CLIENT_ASSERTION_TYPE, clientAssertionType);
+        body.add(OAuth2ParameterNames.CLIENT_ASSERTION, jwt);
+
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED.toString());
+
+        URI url = UriComponentsBuilder.fromUriString(tokenUri).build().toUri();
+        RequestEntity<MultiValueMap<String, String>> request = new RequestEntity<>(body, headers, HttpMethod.POST, url);
+        ResponseEntity<String> res = restTemplate.exchange(request, String.class);
+        ObjectMapper om = new ObjectMapper();
+        om.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+        TokenResponse tr = om.readValue(res.getBody(), TokenResponse.class);
+        return tr.accessToken;
+    }
+
+
+}
--- user-restricted-separate-auth-tutorials/nhs-login/java/src/main/java/nhsd/apim/auth/WebSecurityConfig.java
+++ user-restricted-separate-auth-tutorials/nhs-login/java/src/main/java/nhsd/apim/auth/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package nhsd.apim.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +24,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/", "/home", "/error", "/oauth2/authorization/**", "/webjars/**").permitAll()
+                        .antMatchers("/", "/home", "/error","/login/**", "/oauth2/authorization/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
 //                                .exceptionHandling(e -> e
